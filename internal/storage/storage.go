5b350d73e76dc996a6e981f6481905d1e5eeb902
Spear5030 <spear5030@gmail.com>
2023-01-24 19:14:38 +0300

    tx for postwithdraw

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -143,14 +143,20 @@ func (pgs *storage) GetWithdrawn(ctx context.Context, userID int) (withdrawn flo
 func (pgs *storage) PostWithdraw(ctx context.Context, userID int, order string, sum float64) error {
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
+	tx, err := pgs.db.Begin()
+	if err != nil {
+		pgs.logger.Debug("update TX error" + err.Error())
+		return err
+	}
+	defer tx.Rollback()
 
 	query := `update users set  balance = balance - $1
              		from orders o 
 				where id= o.user_id
 			returning balance;`
-	res := pgs.db.QueryRowContext(ctx, query, sum)
+	res := tx.QueryRowContext(ctx, query, sum)
 	var b float64
-	err := res.Scan(&b)
+	err = res.Scan(&b)
 	pgs.logger.Debug("Post returning balance", zap.Float64("b", b))
 	if err != nil {
 		pgs.logger.Debug(err.Error())
@@ -159,7 +165,12 @@ func (pgs *storage) PostWithdraw(ctx context.Context, userID int, order string, 
 
 	query = `insert into withdrawals(number,user_id, sum)  
        			values($1,$2,$3);`
-	_, err = pgs.db.ExecContext(ctx, query, order, userID, sum)
+	_, err = tx.ExecContext(ctx, query, order, userID, sum)
+	if err != nil {
+		pgs.logger.Debug("update TX error" + err.Error())
+		return err
+	}
+	err = tx.Commit()
 	if err != nil {
 		pgs.logger.Debug("update TX error" + err.Error())
 		return err
